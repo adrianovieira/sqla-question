2cc7e186eea642a193d8422eb332974307a20512
Adriano Vieira <adriano.svieira@gmail.com>
2025-07-02 22:30:24 -0300

    Embarca ormbase no model para simplificar leitura do código
    - adiciona flag python para não criar bytecode

--- surveys_model.py
+++ surveys_model.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 
-from base import ORMBase
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, MetaData, String
+from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+metadata_obj = MetaData(schema="analysis")
+
+
+class ORMBase(metaclass=DeclarativeMeta):
+    __abstract__ = True
+
+    registry = registry(metadata=metadata_obj)
+    metadata = metadata_obj
+
+    __init__ = registry.constructor
 
 
 class MVSurveysStatus(ORMBase):
