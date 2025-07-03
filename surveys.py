8b2e096000752b49baf3d2baee3b3de30a7aaac0
Adriano Vieira <adriano.svieira@gmail.com>
2025-07-03 10:14:29 -0300

    Unifica scripts em um só para "facilitar" análise.

--- surveys.py
+++ surveys.py
@@ -1,7 +1,72 @@
-import sqlalchemy as sa
+import logging
+from datetime import datetime
 
-from orm import ORMUnitOfWork
-from surveys_model import MVSurveysStatus
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+logger = logging.getLogger()
+
+
+class ORMUnitOfWork:
+    session_factory: orm.sessionmaker
+    session: orm.Session
+
+    USER_NAME: str = "surveys"
+    USER_PASS: str = "notsecure"
+    DB_NAME: str = "surveys"
+    DB_SCHEMA: str = "analysis"
+    DB_HOST: str = "pg-server"
+    DB_PORT: int = 5432
+    DRIVER: str = "psycopg"
+    TYPE: str = "postgresql"
+
+    @property
+    def _db_url(self):
+        return f"{self.TYPE}+{self.DRIVER}://{self.USER_NAME}:{self.USER_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    def _sessionmaker(self):
+        _create_engine = sa.create_engine(self._db_url, echo=True)
+        return orm.sessionmaker(bind=_create_engine)
+
+    def __init__(self):
+        super().__init__()
+        self.session_factory = self._sessionmaker()
+        self.session = None
+
+    def __enter__(self):
+        self.session: orm.scoped_session = orm.scoped_session(self.session_factory)
+        return self.session
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.session.close()
+
+        if exception_type and issubclass(exception_type, SQLAlchemyError):
+            logger.error(
+                "ORM SQLAlchemy error. Type {}, Value {}".format(
+                    exception_type, exception_value
+                ),
+                exc_info=traceback,
+            )
+            raise SQLAlchemyError(exception_type, exception_value, traceback)
+
+
+class ORMBase(metaclass=DeclarativeMeta):
+    __abstract__ = True
+
+    registry = orm.registry(metadata=sa.MetaData(schema="analysis"))
+    metadata = sa.MetaData(schema="analysis")
+
+    __init__ = registry.constructor
+
+
+class MVSurveysStatus(ORMBase):
+    __tablename__ = "mview_surveys_loaded_at_status"
+
+    loaded_at: orm.Mapped[datetime] = orm.mapped_column(primary_key=True)
+    status: orm.Mapped[str] = orm.mapped_column(sa.String(15))
+    count: orm.Mapped[int] = orm.mapped_column(sa.Integer)
 
 
 def get_surveys_by_loaded_at():
@@ -9,8 +74,6 @@ def get_surveys_by_loaded_at():
     sql_text = sa.text(
         "SELECT * FROM analysis.mview_surveys_loaded_at_status ORDER BY loaded_at"
     )
-
-    # print(sql_model, sql_text, sep="\n\n", end="\n\n")
 
     with ORMUnitOfWork() as db:
         result_model = db.execute(sql_model).all()
@@ -24,7 +87,7 @@ def get_surveys_by_loaded_at():
         " sa.text('select ...')",
         sep="\t",
     )
-    while count < 30:
+    while count < 20:
         print(
             f"  {count}  |",
             f"{result_model[count][0].loaded_at} => {result_model[count][0].status}",
